810b8c9300e8359dc6c38e19b7c28cb5f0d72cb8
Theo Coombes <theocoombes06@gmail.com>
2021-04-16 23:05:15 +0100

    fix name.py

--- name.py
+++ name.py
@@ -1,12 +1,12 @@
-from random_word import RandomWords
+import requests
 import random
-
-r = RandomWords()
+import json
 
 def new():
     try:
-        f = r.get_random_word(includePartOfSpeech="verb", hasDictionaryDef="true", minDictionaryCount=3).lower()
-        s = r.get_random_word(includePartOfSpeech="noun", hasDictionaryDef="true", minDictionaryCount=3).lower()
+        words = requests.get("https://random-word-api.herokuapp.com/word?number=2&swear=0").json()
+        f = words[0].lower()
+        s = words[1].lower()
         return f + "-" + s + "-" + str(random.randint(0, 999))
     except:
         return new()
