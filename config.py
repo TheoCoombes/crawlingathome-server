167bc32ebcb97af53a325295ff52e185625d4900
Theo Coombes <theocoombes06@gmail.com>
2021-06-01 20:40:24 +0100

    new token system; new ETA system; cleanup of code

--- config.py
+++ config.py
@@ -4,5 +4,9 @@
 HOST = "0.0.0.0"
 PORT = 80
 
-# NODE CONFIG
-IDLE_TIMEOUT = 5400 # (90 minutes)
+# WORKER CONFIG
+IDLE_TIMEOUT = 5400 # The interval until a worker is kicked for being idle. (90 minutes)
+
+# ETA CALCULATION
+AVERAGE_INTERVAL = 1800 # The interval for each measurement of the averages to take place. (30 minutes)
+AVERAGE_DATASET_LENGTH = 50 # The maximum amount of measurements for the averages until older measurements are discarded.
