dc040090919d79fec2366d5d3ab72d0312dd94bb
Theo Coombes <theocoombes06@gmail.com>
2021-06-01 20:46:33 +0100

    move to offline random word selection

--- name.py
+++ name.py
@@ -1,12 +1,11 @@
-import requests
 import random
 import json
 
+with open("words.json", "r") as f:
+    words = json.load(f)
+
 def new():
-    try:
-        words = requests.get("https://random-word-api.herokuapp.com/word?number=2&swear=0").json()
-        f = words[0].lower()
-        s = words[1].lower()
-        return f + "-" + s + "-" + str(random.randint(0, 999))
-    except:
-        return new()
+    words = [random.choice(words) for i in range(2)]
+    f = words[0].lower()
+    s = words[1].lower()
+    return f + "-" + s + "-" + str(random.randint(0, 999))
