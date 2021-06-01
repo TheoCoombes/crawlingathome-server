660e19fb10b7224bb247051708a25e5083049a33
Theo Coombes <theocoombes06@gmail.com>
2021-06-01 23:16:22 +0100

    fix name error

--- name.py
+++ name.py
@@ -2,10 +2,10 @@ import random
 import json
 
 with open("words.json", "r") as f:
-    words = json.load(f)
+    db = json.load(f)
 
 def new():
-    words = [random.choice(words) for i in range(2)]
+    words = [random.choice(db) for i in range(2)]
     f = words[0].lower()
     s = words[1].lower()
     return f + "-" + s + "-" + str(random.randint(0, 999))
