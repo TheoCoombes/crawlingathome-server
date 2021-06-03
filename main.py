d0636a6076a1bf90f8186a1486c4c9441c6c77a4
Theo Coombes <theocoombes06@gmail.com>
2021-06-03 17:51:56 +0100

    add dependency

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException
 from fastapi.responses import HTMLResponse, PlainTextResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
