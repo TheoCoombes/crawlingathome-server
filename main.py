3ba170176b53bc93b222e11db331d9dda73b914f
Theo Coombes <theocoombes06@gmail.com>
2021-06-03 18:42:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ from config import HOST, PORT, IDLE_TIMEOUT, AVERAGE_INTERVAL, AVERAGE_DATASET_L
 
 
 app = FastAPI()
-app.mount(Path("static"), StaticFiles(directory="static"), name="static")
+app.mount("/static/", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 
