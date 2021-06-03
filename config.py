16256bf54fa1417dd3fc41e6f1edcb6614e6fc5a
Theo Coombes <theocoombes06@gmail.com>
2021-06-03 14:55:22 +0100

    add admin to config

--- config.py
+++ config.py
@@ -10,3 +10,6 @@ IDLE_TIMEOUT = 5400 # The interval until a worker is kicked for being idle. (90 
 # ETA CALCULATION
 AVERAGE_INTERVAL = 1800 # The interval for each measurement of the averages to take place. (30 minutes)
 AVERAGE_DATASET_LENGTH = 50 # The maximum amount of measurements for the averages until older measurements are discarded.
+
+# ADMIN
+ADMIN_IP = "<YOUR IP HERE>"
