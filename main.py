53ec7db0d3ed5513964dc3a8fed0ba82d4038b47
Theo Coombes <theocoombes06@gmail.com>
2021-06-20 00:09:20 +0100

    add `/api/validateWorker`

--- main.py
+++ main.py
@@ -281,6 +281,11 @@ async def new(nickname: str):
     return {"display_name": display_name, "token": uuid}
 
 
+@app.get('/api/validateWorker', response_class=PlainTextResponse)
+async def validate(token: str):
+    return str(token in s.clients)
+
+
 @app.post('/api/newJob')
 async def newJob(inp: Optional[TokenInput] = None):
     if not inp:
