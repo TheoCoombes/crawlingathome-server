daea7cb4133757091c6c2aeedf6ecc8034d06f14
Theo Coombes <theocoombes06@gmail.com>
2021-06-27 20:27:51 +0100

    add 3-staged worker support to store

--- store.py
+++ store.py
@@ -1,70 +1,14 @@
-from multiprocessing.managers import SyncManager
-# from config import STORE_PASSWORD, STORE_PORT
 import json
-
-class Manager(SyncManager):
-    pass
 
 class DataLoader:
     def __init__(self, host=False):
         self._load()
         
-        #if host:
-        #    self._load()
-        #    self._host()
-        #else:
-        #    self._connect()
-    
-    
-#     def _host(self):
-#         Manager.register("shard_info", self.shard_info)
-#         Manager.register("open_jobs", self.open_jobs)
-#         Manager.register("closed_jobs", self.closed_jobs)
-#         Manager.register("leaderboard", self.leaderboard)
-#         Manager.register("pending_jobs", self.pending_jobs)
-#         Manager.register("total_jobs", self.total_jobs)
-#         Manager.register("total_pairs", self.total_pairs)
-#         Manager.register("worker_cache", self.worker_cache)
-#         Manager.register("jobs_remaining", self.jobs_remaining)
-#         Manager.register("completion", self.completion)
-#         Manager.register("progress_str", self.progress_str)
-#         Manager.register("eta", self.eta)
-        
-#         self.manager = Manager(("127.0.0.1", STORE_PORT), authkey=STORE_PASSWORD.encode())
-#         self.manager.start()
-    
-#     def _connect(self):
-#         self.manager = Manager(("127.0.0.1", STORE_PORT), authkey=STORE_PASSWORD.encode())
-#         self.manager.connect()
-        
-#         Manager.register("shard_info")
-#         Manager.register("open_jobs")
-#         Manager.register("closed_jobs")
-#         Manager.register("leaderboard")
-#         Manager.register("pending_jobs")
-#         Manager.register("total_jobs")
-#         Manager.register("total_pairs")
-#         Manager.register("worker_cache")
-#         Manager.register("jobs_remaining")
-#         Manager.register("completion")
-#         Manager.register("progress_str")
-#         Manager.register("eta")
-        
-#         self.shard_info = self.manager.shard_info()
-#         self.open_jobs = self.manager.open_jobs()
-#         self.closed_jobs = self.manager.closed_jobs()
-#         self.leaderboard = self.manager.leaderboard()
-#         self.pending_jobs = self.manager.pending_jobs()
-#         self.total_jobs = self.manager.total_jobs()
-#         self.total_pairs = self.manager.total_pairs()
-#         self.worker_cache = self.manager.worker_cache()
-#         self.jobs_remaining = self.manager.jobs_remaining()
-#         self.completion = self.manager.completion()
-#         self.progress_str = self.manager.progress_str()
-#         self.eta = self.manager.eta()
 
     def _load(self):
-        self.clients = {}
+        self.hybrid_clients = {}
+        self.cpu_clients = {}
+        self.gpu_clients = {}
 
         with open("jobs/shard_info.json", "r") as f:
             self.shard_info = json.load(f)
