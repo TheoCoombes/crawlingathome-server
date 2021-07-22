5e2d9926b4bf295c244e633c124037dfb407ac1b
Theo Coombes <theocoombes06@gmail.com>
2021-07-23 00:01:04 +0100

    cleanup script

--- main.py
+++ main.py
@@ -217,10 +217,10 @@ async def lookup_wat(inp: LookupWatInput):
             }
         ])
     
-    if len(shards) == 0:
+    if len(body) == 0:
         return {"status": "failed", "detail": "All shards have already been completed by another worker."}
     else:
-        return {"status": "success", "shards": shards}
+        return {"status": "success", "shards": body}
     
     
 @app.post('/custom/markasdone')
@@ -228,11 +228,10 @@ async def custom_markasdone(inp: MarkAsDoneInput):
     if inp.password != ADMIN_PASSWORD:
         return {"status": "failed", "detail": "Invalid password."}
     
-    existed = 0
     shards = await Job.filter(number__in=inp.shards, closed=False, pending=False)
+    existed = await shards.count()
     
-    existed = shards.count()
-    await shards.update(closed=True, pending=False, gpu=False, completor=inp.nickname)
+    await shards.update(closed=True, pending=False, completor=inp.nickname)
     
     if existed > 0:
         user = await Leaderboard.get_or_create(nickname=inp.nickname)
@@ -242,13 +241,9 @@ async def custom_markasdone(inp: MarkAsDoneInput):
         
         await user.update(job_count=job_count, pairs_scraped=pairs_scraped)
 
-        await s.redis.incrby("total_pairs", amount=inp.count)
-
-        await s.redis.incrby("jobs_remaining", amount=-existed)
-
-        s.completion = (len(s.closed_jobs) / s.total_jobs) * 100
-        s.progress_str = f"{len(s.closed_jobs):,} / {s.total_jobs:,}"
-        
+        await redis.incrby("total_pairs", amount=inp.count)
+        await redis.incrby("job_count", amount=-existed)
+ 
         return {"status": "success"}
     else:
         return {"status": "failed", "detail": "All shards have already been completed by another worker."}
@@ -265,18 +260,19 @@ async def new(nickname: str, type: Optional[str] = "HYBRID"):
     uuid = str(uuid4())
     ctime = int(time())
 
-    worker_data = {
-        "uuid": uuid,
-        "type": type,
-        "user_nickname": nickname,
-        "progress": "Initialized",
-        "jobs_completed": 0,
-        "first_seen": ctime,
-        "last_seen": ctime,
-        "shard": None
+    await Client.create(
+        uuid=uuid,
+        type=type,
+        user_nickname=nickname,
+        progress="Initialized",
+        jobs_completed=0,
+        first_seen=ctime,
+        last_seen=ctime,
+        shard=None
     }
     
-    await Client.create(**worker_data)
+    await redis.incrby("clients")
+    await redis.incrby(f"{type}_clients")
 
     return {"display_name": uuid, "token": uuid, "upload_address": choice(UPLOAD_URLS)}
 
@@ -303,17 +299,17 @@ async def newJob(inp: TokenInput):
     except:
         raise HTTPException(status_code=500, detail="The server could not find this worker. Did the server just restart?")
     
-    if client.shard is not None:
-        await client.shard.update(pending=False, completor=None)
-    
+    if client.shard is not None and client.shard.pending:
+        await client.shard.update(pending=False)
+
     if inp.type == "GPU":         
         try:
             job = await Job.filter(pending=False, closed=False, gpu=True).first()
         except:
             raise HTTPException(status_code=503, detail="No new GPU jobs available. Keep retrying, as GPU jobs are dynamically created.")
         
-        await job.update(pending=True, completor=inp.token)
-        await client.update(progress="Recieved new job", shard=job, last_seen=int(time()))
+        await job.update(pending=True)
+        await client.update(shard=job, progress="Recieved new job", last_seen=int(time()))
         
         return {"url": job.gpu_url, "start_id": job.start_id, "end_id": job.end_id, "shard": job.shard}
     else:
@@ -322,8 +318,8 @@ async def newJob(inp: TokenInput):
         except:
             raise HTTPException(status_code=503, detail="No new GPU jobs available. Keep retrying, as GPU jobs are dynamically created.")
         
-        await job.update(pending=True, completor=inp.token)
-        await client.update(progress="Recieved new job", shard=job, last_seen=int(time()))
+        await job.update(pending=True)
+        await client.update(shard=job, progress="Recieved new job", last_seen=int(time()))
         
         return {"url": job.url, "start_id": job.start_id, "end_id": job.end_id, "shard": job.shard}
 
@@ -334,9 +330,9 @@ async def jobCount(type: Optional[str] = "HYBRID"):
         raise HTTPException(status_code=400, detail=f"Invalid worker type. Choose from: {types}.")
         
     if type == "GPU":
-        return str(await Job.filter(gpu=True, closed=False, pending=False).count())
+        return str(await redis.get("job_count_gpu"))
     else:
-        return str(await Job.filter(gpu=False, closed=False, pending=False).count())
+        return str(await redis.get("job_count"))
 
 
 @app.post('/api/updateProgress', response_class=PlainTextResponse)
@@ -368,17 +364,14 @@ async def markAsDone(inp: TokenCountInput):
         raise HTTPException(status_code=500, detail="This job has already been marked as completed!")
     
     if inp.type == "CPU":
-        if inp.url is None or inp.start_id is None or inp.end_id is None or inp.shard is None:
-            raise HTTPException(status_code=500, detail="The worker did not submit valid input data.")
+        if inp.url is None:
+            raise HTTPException(status_code=500, detail="The worker did not submit valid download data.")
         
         await client.shard.update(gpu=True, pending=False, gpu_url=inp.url, cpu_completor=client.user_nickname)
         await client.update(shard=None, progress="Completed Job", last_seen=int(time()), jobs_completed=(client.jobs_completed+1))
 
-        user = await Leaderboard_CPU.get_or_create(nickname=client.user_nickname)
-        
-        job_count = user.job_count += 1
-        
-        await user.update(job_count=job_count)
+        user = await Leaderboard_CPU.get_or_create(nickname=client.user_nickname)    
+        await user.update(job_count=(user.job_count + 1))
         
         return "success"
     else:
@@ -388,12 +381,8 @@ async def markAsDone(inp: TokenCountInput):
         await client.shard.update(closed=True, pending=False, completor=client.user_nickname)
         await client.update(shard=None, progress="Completed Job", last_seen=int(time()), jobs_completed=(client.jobs_completed+1))
 
-        user = await Leaderboard.get_or_create(nickname=client.user_nickname)
-        
-        job_count = user.job_count += 1
-        pairs_scraped = user.pairs_scraped += inp.count
-        
-        await user.update(job_count=job_count, pairs_scraped=pairs_scraped)
+        user = await Leaderboard.get_or_create(nickname=client.user_nickname)    
+        await user.update(job_count=(user.job_count + 1), pairs_scraped=(user.pairs_scraped + inp.count))
 
         return "success"
 
@@ -487,37 +476,6 @@ async def calculate_eta():
             s.eta = _format_time(length)
         else:
             s.eta = "Finished"
-
-        
-async def save_jobs_leaderboard():
-    a = len(s.open_jobs)
-    b = len(s.closed_jobs)
-    c = sum([s.leaderboard[i][1] for i in s.leaderboard])
-    d = len(s.open_gpu)
-    while True:
-        await asyncio.sleep(300)
-        
-        w = len(s.open_jobs)
-        if a != w:
-            async with aiofiles.open("jobs/open.json", "w") as f:
-                await f.write(json.dumps(s.open_jobs))
-        x = len(s.closed_jobs)
-        if b != x:
-            async with aiofiles.open("jobs/closed.json", "w") as f:
-                await f.write(json.dumps(s.closed_jobs))
-        y = sum([s.leaderboard[i][1] for i in s.leaderboard])
-        if c != y:
-            async with aiofiles.open("jobs/leaderboard.json", "w") as f:
-                await f.write(json.dumps(s.leaderboard))
-        z = len(s.open_gpu)
-        if d != z:
-            async with aiofiles.open("jobs/open_gpu.json", "w") as f:
-                await f.write(json.dumps(s.open_gpu))
-
-        a = w
-        b = x
-        c = y
-        d = z
         
 
 # FASTAPI UTILITIES START ------ 
@@ -525,28 +483,11 @@ async def save_jobs_leaderboard():
     
 @app.on_event('startup')
 async def app_startup():
-    if __name__ == "__main__":
+    if __name__ == "__main__": # TODO if worker == 0
         asyncio.create_task(check_idle())
         asyncio.create_task(calculate_eta())
-        asyncio.create_task(save_jobs_leaderboard())
 
-  
-@app.on_event('shutdown')
-async def shutdown_event():
-    if __name__ == "__main__":
-        async with aiofiles.open("jobs/open.json", "w") as f:
-            await f.write(json.dumps(s.open_jobs))
-        
-        async with aiofiles.open("jobs/closed.json", "w") as f:
-            await f.write(json.dumps(s.closed_jobs))
-        
-        async with aiofiles.open("jobs/leaderboard.json", "w") as f:
-            await f.write(json.dumps(s.leaderboard))
-        
-        async with aiofiles.open("jobs/open_gpu.json", "w") as f:
-            await f.write(json.dumps(s.open_gpu))
 
-        
 @app.exception_handler(StarletteHTTPException)
 async def http_exception_handler(request, exc):
     return PlainTextResponse(str(exc.detail), status_code=exc.status_code)
@@ -565,4 +506,4 @@ register_tortoise(
 
 
 if __name__ == "__main__":
-    print("From v3.0.0, you can no longer run this script directly from Python. Use gunicorn/uvicorn directly, using \"main:app\" as the server.")
+    print("From v3.0.0, you can no longer run this script directly from Python. Call gunicorn/uvicorn directly from the terminal, using \"main:app\" as the server.")
