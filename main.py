fcbd115082585877d500733a820e80aeb1da9074
Theo Coombes <theocoombes06@gmail.com>
2021-07-24 17:59:34 +0100

    fix variable name

--- main.py
+++ main.py
@@ -201,7 +201,7 @@ async def worker_data(type: str, display_name: str):
         c = await Client.get(display_name=display_name, type=type).prefetch_related("shard")
         return {
             "display_name": c.display_name,
-            "shard_number": c.shard.number if client.shard else "N/A",
+            "shard_number": c.shard.number if c.shard else "N/A",
             "progress": c.progress,
             "jobs_completed": c.jobs_completed,
             "first_seen": c.first_seen,
