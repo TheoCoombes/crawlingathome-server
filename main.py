e0017a257ee07ad1ef9709fb4c64f1699fd91b6e
Theo Coombes <theocoombes06@gmail.com>
2021-07-24 21:18:45 +0100

    fix check_idle 3

--- main.py
+++ main.py
@@ -568,7 +568,7 @@ async def check_idle():
         await asyncio.sleep(30) # TODO increase to 300
         t = int(time()) - IDLE_TIMEOUT
         
-        await Client.filter(last_seen__lte=t, shard__not=None).select_related("shard").update(pending=False)
+        await Client.filter(last_seen__lte=t).select_related("shard").update(pending=False)
         await Client.filter(last_seen__lte=t).delete()
 
         
