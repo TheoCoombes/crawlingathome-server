4d8149e76c008c4f416e2929c35b026df5da09a7
Theo Coombes <theocoombes06@gmail.com>
2021-07-29 02:16:33 +0100

    finish on cache

--- cache.py
+++ cache.py
@@ -1,14 +1,16 @@
 import asyncio
-import aioredis
 from os import getpid
 from time import time
 from typing import Optional, Tuple
 from config import PAGE_CACHE_EXPIRY
 
+from aioredis.utils import from_url
+from aioredis.client import Redis
+
 class _PageCache:
-    def __init__(self, client: aioredis.client.Redis):
+    def __init__(self, client: Redis):
         self._redis = client
-    
+        
     async def exists(self, page) -> bool:
         """ Returns True if page `page` exists in the cache. """
         return bool(await self._redis.exists(page))
@@ -35,14 +37,16 @@ class _PageCache:
             return None, True
         else:
             return body, False
-        
+
+
 class Cache:
     def __init__(self, connection_url: str):
         """ Creates the Redis client instance as well as a `_PageCache` instance for caching webpages. """
-        self.client = aioredis.utils.from_url(connection_url)
+        self.client = from_url(connection_url)
         self.page = _PageCache(self.client)
     
-    async def initPID(self, sleep: bool = True) -> int:
+    
+    async def initPID(self, sleep: bool = True) -> None:
         """ Gets the current process ID, and pushes it to the Redis `workers` list. """
         self.pid = getpid()
         await self.client.rpush("workers", pid)
@@ -51,7 +55,7 @@ class Cache:
             await asyncio.sleep(0.25)
             
         self.worker = await self.client.lpos("workers", self.pid)
-        return self.worker
+    
     
     async def safeExit(self) -> None:
         """ [IMPORTANT] Resets the workers list to allow the server to
