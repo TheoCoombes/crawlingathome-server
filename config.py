9293e31919ac6f81ef0af16185a5f751148f58ce
Theo Coombes <theocoombes06@gmail.com>
2021-07-29 02:05:59 +0100

    cleanup config

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 
 # DATABASE CONFIG
 SQL_CONN_URL = "postgres:///crawlingathome" # Example config for a postgres database. Works with any databases supported by Tortoise ORM.
-REDIS_CONN_URL = "redis://localhost" # The Redis connection URL, used for caching webpages to avoid database strain.
+REDIS_CONN_URL = "redis://127.0.0.1" # The Redis connection URL, used for caching webpages to avoid database strain.
 
 # WORKER CONFIG
-IDLE_TIMEOUT = 1800 # The interval until a worker is kicked for being idle. (3 hours)
+IDLE_TIMEOUT = 1800 # The interval, in seconds, until a worker is kicked for being idle. (default 30 minutes)
 
 # ETA CALCULATION
-AVERAGE_INTERVAL = 900 # The interval for each measurement of the averages to take place. (15 minutes)
+AVERAGE_INTERVAL = 900 # The interval for each measurement of the averages to take place. (default 15 minutes)
 AVERAGE_DATASET_LENGTH = 10 # The maximum amount of measurements for the averages until older measurements are discarded.
 
 # ADMIN
