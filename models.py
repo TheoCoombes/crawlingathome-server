1e9fdf0ea8c553e50d394514a647f41d87ad9683
Theo Coombes <theocoombes06@gmail.com>
2021-08-09 21:13:00 +0100

    rand

--- models.py
+++ models.py
@@ -109,7 +109,7 @@ WHERE "number" IN
     (
      SELECT "number" FROM "job" 
      WHERE pending=false AND closed=false AND gpu=true 
-     ORDER BY "number" ASC LIMIT 1
+     ORDER BY RAND() LIMIT 1
      FOR UPDATE SKIP LOCKED
     )
   AND pending=false AND closed=false AND gpu=true
@@ -123,7 +123,7 @@ WHERE "number" IN
     (
      SELECT "number" FROM "job" 
      WHERE pending=false AND closed=false AND gpu=false 
-     ORDER BY "number" ASC LIMIT 1
+     ORDER BY RAND() LIMIT 1
      FOR UPDATE SKIP LOCKED
     )
   AND pending=false AND closed=false AND gpu=false
