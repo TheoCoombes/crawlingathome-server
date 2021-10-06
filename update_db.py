2b4312bbd362cc243854cd711e790c48b3c043d7
Theo Coombes <theocoombes06@gmail.com>
2021-10-06 13:58:33 +0100

    fix 2

--- update_db.py
+++ update_db.py
@@ -52,6 +52,17 @@ async def init():
         
         jobs.append(job)
     
+    # Dedupe
+    seen = set()
+    new_jobs = []
+    for job in jobs:
+        if job.number not in seen:
+            new_jobs.append(job)
+            seen.add(job.number)
+    jobs = new_jobs
+    
+    jobs = sorted(jobs, key=lambda x: x.number) # Sort
+    
     jobs = sorted(jobs, key=lambda x: x.number) # Sort
     
     
